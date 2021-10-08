244aedb06b28760d1021bd2562a73b50589b8e14
David Sans <dsans79@yahoo.es>
2021-10-08 11:54:50 +0200

    findByManufacturer add

--- M2_Testing_David_Sans/src/main/java/com/example/M2_Testing_David_Sans/repositories/SmartPhonesRepository.java
+++ M2_Testing_David_Sans/src/main/java/com/example/M2_Testing_David_Sans/repositories/SmartPhonesRepository.java
@@ -3,8 +3,10 @@ package com.example.M2_Testing_David_Sans.repositories;
 import com.example.M2_Testing_David_Sans.entities.SmartPhones;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface SmartPhonesRepository extends JpaRepository<SmartPhones, Long>{
-
-
+    List<SmartPhones> findByManufacturer(String manufacturer);
 }
